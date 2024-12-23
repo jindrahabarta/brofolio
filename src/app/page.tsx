27a7d03979bcd05af6140ca318f70326f53d6954
lubahabarta <habarta@deepvision.cz>
2024-12-23 19:45:50 +0100

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
             <AboutUs />
             <Projects />
             <FAQ />
-            <Contact />
             <Technology />
             <Contact />
         </>
