b9704997b13c87b0e84a996f7b6d4c35235376b2
jindrahabarta <jindriskuv@email.cz>
2024-12-25 12:16:44 +0100

    navigation progress

--- src/_components/Navigation/HamburgerIcon.tsx
+++ src/_components/Navigation/HamburgerIcon.tsx
@@ -15,10 +15,17 @@ const HamburgerIcon = ({
                 className='hamburgerIcon btnFilter group w-14 h-14 rounded-lg cursor-pointer flex items-center justify-center'
             >
                 <div className='flex flex-col justify-center items-center gap-2'>
-                    <span className='rounded-full w-8 h-[2px] bg-white/50 group-hover:bg-white/90 duration-200 delay-200'></span>
-                    <span className='rounded-full w-8 h-[2px] bg-white/65 group-hover:bg-white/90 duration-200 delay-100'></span>
-                    <span className='rounded-full w-8 h-[2px] bg-white/75 group-hover:bg-white/90 duration-200 '></span>
+                    <div className='rounded-full w-8 h-[2px] bg-white/50 '>
+                        <div className='rounded-full h-full w-0 bg-white group-hover:w-full duration-200'></div>
+                    </div>
+                    <div className='rounded-full w-8 h-[2px] bg-white/65'>
+                        <div className='rounded-full h-full w-0 bg-white group-hover:w-full duration-200 delay-100'></div>
+                    </div>
+                    <div className='rounded-full w-8 h-[2px] bg-white/75 '>
+                        <div className='rounded-full h-full w-0 bg-white group-hover:w-full duration-200 delay-200'></div>
+                    </div>
                 </div>
+                {/* <figure className='bg-yellow-400 absolute w-full h-full -z-50 opacity-55 blur-xl'></figure> */}
             </button>
         )
     else if (variant === 'close')
