3c2bb0794b59c5bbf987bf7c14f537f081d6dd99
lubahabarta <lubahabarta@gmail.com>
2025-05-06 17:37:55 +0200

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { Metadata } from 'next'
-import AboutUs from './_components/Sections/AboutUs/AboutUs'
-import Contact from './_components/Sections/Contact/Contact'
-// import FAQ from './_components/Sections/FAQ/FAQ'
-import Hero from './_components/Sections/Hero/Hero'
-// import Process from './_components/Sections/Process/Process'
-import Projects from './_components/Sections/Projects/Projects'
-import Loader from '@/_components/Loader/Loader'
 import LoaderProvider from '@/_components/Loader/LoaderProvider'
-// import Technology from './_components/Sections/Technology/Technology'
 
 export const metadata = {
     title: {
