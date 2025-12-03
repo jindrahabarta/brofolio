1437b57457fb425eb7322d7c57fa65b6f7765504
jindrahabarta <jindriskuv@email.cz>
2025-12-03 13:00:14 +0100

    page not found translation

--- src/app/[locale]/page-not-found/page.tsx
+++ src/app/[locale]/page-not-found/page.tsx
@@ -1,44 +1,57 @@
 import { Link } from '@/i18n/navigation'
 import { Metadata } from 'next'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
-export const metadata: Metadata = {
-    title: 'Ooops, tato stránka neexistuje',
-    description:
-        'Tvoříme unikátní weby na zakázku. Náš cíl je doručovat weby vyladěné do posledního pixelu, ovšem ne na úkor jejich výkonu. Design a rychlost jsou klíčem.',
-    keywords: ['Zakázkové weby', 'Tvorba webů', 'Motion labs'],
-    authors: [{ name: 'Motionlabs', url: 'https://motionlabs.cz' }],
-    twitter: {
-        card: 'summary_large_image',
-    },
-    openGraph: {
-        title: 'Motionlabs - Web development',
-        description: '',
-        images: [''],
-    },
-    robots: {
-        index: false,
-        follow: false,
-    },
+type Props = {
+    params: Promise<{ locale: string }>
 }
 
-const page = () => {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { locale } = await params
+
+    const t = (await import(`../../../i18n/locales/${locale}/layout.json`))
+        .default
+
+    return {
+        title: `Ooops, ${t.notFound.text}`,
+        description: t.seo.description,
+        keywords: t.seo.keywords,
+        authors: [{ name: 'Motionlabs', url: 'https://motionlabs.cz' }],
+        twitter: {
+            card: 'summary_large_image',
+        },
+        openGraph: {
+            title: 'Motionlabs - Web development',
+            description: t.seo.description,
+            images: '/brand/open-graph.png',
+        },
+        robots: {
+            index: false,
+            follow: false,
+        },
+    }
+}
+
+const PageNotFound = () => {
+    const t = useTranslations('notFound')
+
     return (
         <section className='text-white pt-9 h-screen bg-black overflow-hidden flex flex-col items-center justify-center'>
             <h1 className=' bottom-4 text-[4vw] left-4 text-center'>
                 <strong>404</strong>
             </h1>
-            <p>Tato stránka neexistuje</p>
+            <p>{t('text')}</p>
 
             <Link
                 aria-label='Navigation link to home'
                 className='underline mt-4 hover:no-underline'
                 href={'/'}
             >
-                Jít domů
+                {t('button')}
             </Link>
         </section>
     )
 }
 
-export default page
+export default PageNotFound
